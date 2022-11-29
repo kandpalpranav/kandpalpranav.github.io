ae7852940bade81744c7c5efa6f6792e23469a02
kandpalpranav <55237329+kandpalpranav@users.noreply.github.com>
2022-11-29 17:02:26 +0100

    Update Webcomponent.js

--- CustomWidget/Webcomponent.js
+++ CustomWidget/Webcomponent.js
@@ -22,7 +22,7 @@
 
         fireChanged() {
             console.log("OnClick Triggered");      
-            var measures = window.sap.raptr.getEntries();
+            var measures = window.performance.getEntries();
             var exportName = 'RaptrMeasures_' +  Date.now().toString() + '.json';
             var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(measures));
             var downloadAnchorNode = document.createElement('a');
